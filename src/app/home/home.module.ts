c7132ab42df670f4c890872ed355a14474475644
michaeladebisi <michael.adebisi@centrica.com>
2020-06-21 15:34:55 +0100

    adding restrr app function

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
-import { HomePage } from './home.page';
+import { RouterModule } from '@angular/router';
 
-import { HomePageRoutingModule } from './home-routing.module';
+import { HomePage } from './home.page';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import {ProgressBarModule} from "angular-progress-bar"
 
 
 @NgModule({
@@ -12,7 +14,24 @@ import { HomePageRoutingModule } from './home-routing.module';
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    RouterModule.forChild([
+      {
+        path: '',
+        component: HomePage
+      }
+    ]),
+    NgCircleProgressModule.forRoot({
+      // set defaults here
+      radius: 100,
+      outerStrokeWidth: 16,
+      innerStrokeWidth: 8,
+      outerStrokeColor: "#78C000",
+      innerStrokeColor: "#C7E596",
+      animation: false,
+      responsive: true,
+      renderOnClick: false
+    }),
+    
   ],
   declarations: [HomePage]
 })
